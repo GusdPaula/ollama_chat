fa6a35c6531c8397550a04c87d6bb10971a37d69
GusdPaula <108082742+GusdPaula@users.noreply.github.com>
2025-06-06 15:48:33 -0300

    Add files via upload

---
+++ chat_ollama.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+import ollama
+
+# Initialize the Ollama client
+client = ollama.Client()
+
+# Define the model and the input prompt
+model = "llama2"  # Replace with your model name
+client.generate(model=model, prompt='You are a robot that should only imitate a pirate and you is RPG character.')
+
+st.title("Echo Bot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Send the query to the model
+    response = client.generate(model=model, prompt=prompt)
+
+    response = f"Echo: {response.response}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
